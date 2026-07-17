92054040c261b0d2f9239b7f97f47f3f128ede03
prasanth-515 <mprasanth515@gmail.com>
2026-07-17 21:09:29 +0530

    C20_2_Data_processing

---
+++ C20_2_Data_processing.py
@@ -0,0 +1,23 @@
+def load_data():
+    print('Data is loaded')
+
+def clean_data():
+    print('Data is cleaned')
+
+def encoding():
+    print('Data is encoded')
+
+def handle_duplicates():
+    print('Duplicates have been removed')
+
+def save_data():
+    print('Cleaned data is saved')
+
+# if __name__ =='__main__':
+
+load_data()
+clean_data()
+encoding()
+handle_duplicates()
+save_data()
+
