69236b46a51b33fa9b4dba3eec66b52e9aa4bb36
prasanth-515 <mprasanth515@gmail.com>
2026-07-17 21:10:12 +0530

    C20_3_Moduless

---
+++ C20_3_Moduless.py
@@ -0,0 +1,38 @@
+def intro():
+	return "It is a new moudule"
+
+def sho():
+	return " Hellooo "
+
+def add(x,y):
+	return x+y
+
+def sub(x,y):
+	return x-y
+	
+def mul(x,y):
+	return x*y
+
+def div(x,y):
+	return x/y
+
+def get_details():
+	return "It is a user defined module for testing purpose"
+
+print(f'__name__ ---> {__name__}')
+if __name__ == '__main__':
+    print('Hey i got this')
+    x = 10
+    y = 20
+    z = x*y
+    print('MUlitplication is : ')
+elif __name__ == "moduless":
+    print('Hello friends')
+    a = 10
+    b = 20
+    c = a + b
+    print('Addition is ',c)
+    print('Thanks')
+
+
+
